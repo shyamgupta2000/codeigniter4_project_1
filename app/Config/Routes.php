7954c44319a667c3ae09347977ca31ae584a591c
shyamgupta2000 <crackthecode2000@gmail.com>
2024-02-20 16:34:10 +0530

    second commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-<<<<<<< HEAD
 $routes->get('/signup', 'WebsiteController::signup_form', ['as' => 'signup']);
-=======
->>>>>>> 016eef7736c8e18e67cf354edd62146c873c4c7e
